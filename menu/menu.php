626cb32f8c370c82cec4f7781bce3af44c242042
przemek768 <przemek76816@gmail.com>
2021-01-04 13:06:33 +0100

    Strona_przyklad

---
+++ menu/menu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="menu_style.css">
+</head>
+<body>
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">Warsztaty</a>
+  <a href="#">Sklep</a>
+  <a href="#">Forum</a>
+  <a href="#">Kontakt</a>
+  <a href="#">Moje konto</a>
+
+</div>
+<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+<script type="text/javascript" src="menu_js.js"></script>
+   
+</body>
+</html> 
+
+
